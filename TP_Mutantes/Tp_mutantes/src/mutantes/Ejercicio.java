d2025307817bd4f3115f2ec7b52477600e4a51c7
mn204 <mln204manu@gmail.com>
2023-10-30 20:12:17 -0300

    subo primera instancia de tp mutantes

---
+++ TP_Mutantes/Tp_mutantes/src/mutantes/Ejercicio.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mutantes;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Manuel
+ */
+public class Ejercicio {
+    public static void main(String[] args) {
+        
+    }
+    static boolean isMutant(String[] dna){
+    return true;
+    }
+
+    
+}
