65f7718c387e197a36fbb0ba969cd03710945b9c
mn204 <mln204manu@gmail.com>
2023-11-05 19:42:42 -0300

    se agregan metodos de ejercios pasados, se realiza punto 1 del tp

--- TP_integrador2/TP_Integrador2/src/Integrador/CalculoBonoSueldo.java
+++ TP_integrador2/TP_Integrador2/src/Integrador/CalculoBonoSueldo.java
@@ -6,6 +6,7 @@ package Integrador;
 
 import java.util.List;
 import java.util.Scanner;
+import java.time.YearMonth; //obtener anio actual
 
 /**
  *
@@ -13,7 +14,7 @@ import java.util.Scanner;
  */
 public class CalculoBonoSueldo {
     static Scanner sc =new Scanner(System.in);
-    
+    static int anio = YearMonth.now().getYear();
     public static void main(String[] args) {
         
         String[][] haberes = {{"100","Presentismo", "9"} , {"101"," Titulo Profesional" , "9"} , {"102","Horas Extraordinarias", "M"} , {"103","Horas Nocturnas", "M"} , {"104"," Otros Haberes", "M"}};
@@ -22,10 +23,40 @@ public class CalculoBonoSueldo {
         System.out.println("Comenzar carga de bono de Sueldo......");
         System.out.println("---------------------------------------");
          do {            
-            Empleado persona = new Empleado();
-             System.out.println("Ingrese el nombre del empleado: ");
+            Empleado persona = new Empleado();  // instancia de nuevo empleado
+            System.out.println("Ingrese el nombre del empleado: ");
             persona.setNombreEmpleado(sc.nextLine());
-           
+            System.out.println("Ingrese el CUIL del empleado: ");
+            persona.setCuil(sc.nextLong());
+            sc.nextLine();
+            
+             while (true) {  // ciclo para ingresar años y verificar que no hayan errores    
+                 System.out.println("Ingrese el anio de ingreso del empleado: ");
+                 int anioingreso = sc.nextInt();
+                 sc.nextLine();
+                 System.out.println("Ingrese el mes de liquidacion del empleado: ");
+                 int mesliq = sc.nextInt();
+                 sc.nextLine();
+                 System.out.println("Ingrese el anio de liquidacion del empleado: ");
+                 int anioliq = sc.nextInt();
+                 sc.nextLine();
+                 if (anioingreso >=anio || anioliq >= anio) {
+                     System.out.println("Los anios de liquidacion o ingreso superan el anio actual comience nuevamente...");
+                     continue;
+                 }
+                 System.out.println("Ingrese el sueldo basico");
+                 double sueldobasico = sc.nextDouble();
+                 sc.nextLine();
+                 
+                 double montoAntiguedad = (anio - anioingreso) * 0.02;
+                 persona.setMontoAntiguedad(montoAntiguedad);
+                 
+                 BonoSueldo bono = new BonoSueldo();
+                 bono.setEmpleado(persona);
+                 bono.setMesLiquidacion(mesliq);
+                 bono.setAnioLiquidacion(anioliq);
+             }
+            
              
              
              
@@ -37,5 +68,29 @@ public class CalculoBonoSueldo {
         } while (true);
         
         
+    }
+    
+    public static String completarConEspacios(String cadena){
+    int cantidadEspacios = 20-cadena.length();
+    for (int i = 0; i < cantidadEspacios; i++) {
+        cadena+= " ";
+        }
+    return cadena;
+    }
+    public static boolean salidaSubmenu(){
+        boolean respuesta = false;
+        do{
+            System.out.print("(SI/NO): ");
+            String opcion = sc.nextLine();
+            if (opcion.equalsIgnoreCase("si")) {
+                break;
+            } else if(opcion.equalsIgnoreCase("no")) {
+                respuesta = true;
+                break; 
+            }else {
+                System.out.println("Ingrese una respuesta valida!");
+            }
+        } while (true);
+        return respuesta;
     }
 }
