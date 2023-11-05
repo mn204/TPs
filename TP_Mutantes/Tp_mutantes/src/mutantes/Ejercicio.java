2820c7e1b7c3e3ca20cf75bc5892f83100561610
mn204 <mln204manu@gmail.com>
2023-11-05 00:02:03 -0300

    Se realiza la carga de las cadenas  al array, con todos los pasos de verificacion para que no hayan errores de cargado

--- TP_Mutantes/Tp_mutantes/src/mutantes/Ejercicio.java
+++ TP_Mutantes/Tp_mutantes/src/mutantes/Ejercicio.java
@@ -11,7 +11,39 @@ import java.util.Scanner;
  */
 public class Ejercicio {
     public static void main(String[] args) {
-        System.out.println("Ingrese el array de strings:");
+        Scanner sc = new Scanner(System.in);
+        String[] dna = new String[6];
+        System.out.println("Comienzo de programa mutantes....");
+        
+        for (int i = 0; i < 6; i++) {
+            while (true) { // se abre un ciclo para ingresar la cadena
+                System.out.println("Ingrese la fila nro " +  (i+1) + ": ");
+                System.out.println("(Recuerde que solo debe ingresar 6 letras y solo pueden ser: A,T,C,G)");
+                String base= sc.nextLine().toUpperCase();
+                System.out.println("La cadena ingresada es: " + base);
+                if (base.length() == 6){ //verifico que la cadena sea de 6
+                    int letraValida = 0; // comienzo un contador para ver si se cumple la cantidad de letras validas
+                    for (int j = 0; j < 6; j++){
+                        char letra = base.charAt(j);
+                        if (letra == 'A' || letra == 'T' || letra == 'C' || letra == 'G') {
+                            letraValida++; //si se cumple aumenta en 1
+                        }
+                    }
+                    if (letraValida == 6) { // al verificarse que son 6 letras validas se añade la base al array
+                        System.out.println("La base ingresada es valida, añadiendo a la fila.");
+                        dna[i]= base;
+                        break; // se rompe el ciclo while para pasar a la otra posicion del array
+                    } else {
+                        System.out.println("La base ingresada contiene caracteres no validos, comience nuevamente");
+                    }
+                } else {
+                    System.out.println("La cadena no tiene 6 caracteres, comience nuevamente.");
+                }
+            }
+        }
+        for (String string : dna) {
+            System.out.println(string);
+        }
     }
     static boolean isMutant(String[] dna){
     return true;
